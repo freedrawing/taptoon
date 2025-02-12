5aa6faae7d6343b742dee5b1270fe036b951cc18
dllll2 <wlsdud8658@gmail.com>
2025-02-12 12:02:22 +0900

    ♻️Chat 관련 엔티티 설정 수정

--- src/main/java/com/sparta/taptoon/domain/chat/entity/ChatMessage.java
+++ src/main/java/com/sparta/taptoon/domain/chat/entity/ChatMessage.java
@@ -1,23 +1,43 @@
 package com.sparta.taptoon.domain.chat.entity;
 
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "chat_message")
 public class ChatMessage {
-    private Long id;
-    private Long room_id;
-    private Long sender_id;
-    private String message;
-    private Boolean isRead;
-    private Boolean isDeleted;
 
-    @Builder
-    public ChatMessage(Long room_id, Long sender_id, String message, Boolean isRead, Boolean isDeleted) {
-        this.room_id = room_id;
-        this.sender_id = sender_id;
-        this.message = message;
-        this.isRead = isRead;
-        this.isDeleted = isDeleted;
-    }
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false, updatable = false)
+  private Long id;
+
+  @Column(name = "room_id", nullable = false)
+  private Long roomId;
+
+  @Column(name = "sender_id", nullable = false)
+  private Long senderId;
+
+  @Column(name = "message", nullable = false)
+  private String message;
+
+  @Column(name = "is_read", nullable = false)
+  private Boolean isRead;
+
+  @Column(name = "is_deleted", nullable = false)
+  private Boolean isDeleted;
+
+  @Builder
+  public ChatMessage(Long roomId, Long senderId, String message, Boolean isRead,
+                     Boolean isDeleted) {
+    this.roomId = roomId;
+    this.senderId = senderId;
+    this.message = message;
+    this.isRead = isRead;
+    this.isDeleted = isDeleted;
+  }
 }
--- src/main/java/com/sparta/taptoon/domain/chat/entity/ChatRoom.java
+++ src/main/java/com/sparta/taptoon/domain/chat/entity/ChatRoom.java
@@ -1,23 +1,38 @@
 package com.sparta.taptoon.domain.chat.entity;
 
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@Entity
+@Table(name = "chat_room")
 public class ChatRoom {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false, updatable = false)
   private Long id;
+
+  @Column(name = "name", nullable = false)
   private String name;
-  private Long user1_id;
-  private Long user2_id;
-  private Boolean is_deleted;
+
+  @Column(name = "user1_id", nullable = false)
+  private Long user1Id;
+
+  @Column(name = "user2_id", nullable = false)
+  private Long user2Id;
+
+  @Column(name = "is_deleted", nullable = false)
+  private Boolean isDeleted;
 
   @Builder
-  public ChatRoom(String name, Long user1_id, Long user2_id, Boolean is_deleted) {
+  public ChatRoom(String name, Long user1Id, Long user2Id, Boolean isDeleted) {
     this.name = name;
-    this.user1_id = user1_id;
-    this.user2_id = user2_id;
-    this.is_deleted = is_deleted;
+    this.user1Id = user1Id;
+    this.user2Id = user2Id;
+    this.isDeleted = isDeleted;
   }
 }
