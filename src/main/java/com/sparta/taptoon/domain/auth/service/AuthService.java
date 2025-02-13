de3422c8b45984ece171e49d4d6e7168657b1bb5
chk223 <kch23_71@naver.com>
2025-02-13 20:09:38 +0900

    ✨ security, jwt를 이용한 인증 구현

--- src/main/java/com/sparta/taptoon/domain/auth/service/AuthService.java
+++ src/main/java/com/sparta/taptoon/domain/auth/service/AuthService.java
@@ -2,37 +2,48 @@ package com.sparta.taptoon.domain.auth.service;
 
 import com.sparta.taptoon.domain.auth.dto.request.LoginMemberRequest;
 import com.sparta.taptoon.domain.auth.dto.request.SignupMemberRequest;
+import com.sparta.taptoon.domain.auth.dto.response.LoginMemberResponse;
 import com.sparta.taptoon.domain.member.dto.response.MemberResponse;
 import com.sparta.taptoon.domain.member.entity.Member;
 import com.sparta.taptoon.domain.member.repository.MemberRepository;
+import com.sparta.taptoon.global.error.exception.AccessDeniedException;
 import com.sparta.taptoon.global.error.exception.InvalidRequestException;
 import com.sparta.taptoon.global.error.exception.NotFoundException;
+import com.sparta.taptoon.global.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
     private final MemberRepository memberRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
 
     public MemberResponse signUp(SignupMemberRequest request) {
-        Member member = request.toEntity();
+        Member member = request.toEntity(passwordEncoder);
         Member savedMember = memberRepository.save(member);
         return MemberResponse.from(savedMember);
     }
 
-    public String login(LoginMemberRequest request) {
-        Member member = memberRepository.findByEmail(request.email()).orElseThrow(NotFoundException::new);
-        /**
-         * 비밀번호 검증 로직
-         */
-        if (!member.getPassword().equals(request.password())) {
-            throw new InvalidRequestException();
+    public LoginMemberResponse login(LoginMemberRequest request) {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(request.email(), request.password());
+        try{
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+            String accessToken = jwtUtil.generateAccessToken(authentication);
+            String refreshToken = jwtUtil.generateRefreshToken(authentication);
+            return new LoginMemberResponse(accessToken,refreshToken);
+        } catch (BadCredentialsException e) {
+           throw new InvalidRequestException();//exception 처리 필요
+        } catch (Exception e) {
+            throw new AccessDeniedException();//exception 처리 필요
         }
-        /**
-         * 토큰 발급 로직
-         */
-        String accessToken = "accessToken";
-        return accessToken;
     }
 }
