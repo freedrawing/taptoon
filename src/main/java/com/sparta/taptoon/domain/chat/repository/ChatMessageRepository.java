ca43ba9e8ab9fddc990c506714e22c0c9ce22270
dllll2 <wlsdud8658@gmail.com>
2025-02-13 09:54:31 +0900

    ✨ 채팅방의 메시지를 조회하기위한 ChatMessageRepository

---
+++ src/main/java/com/sparta/taptoon/domain/chat/repository/ChatMessageRepository.java
@@ -0,0 +1,14 @@
+package com.sparta.taptoon.domain.chat.repository;
+
+import com.sparta.taptoon.domain.chat.entity.ChatMessage;
+import com.sparta.taptoon.domain.chat.entity.ChatRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+
+    List<ChatMessage> findByChatRoomOrderByCreatedAtAsc(ChatRoom chatRoom);
+}
