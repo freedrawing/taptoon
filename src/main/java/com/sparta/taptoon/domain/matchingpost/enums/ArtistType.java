14428a7bf54aeb60670a5870ca05305f093b0050
seonguk_kang <dnr2144@gmail.com>
2025-02-14 16:44:48 +0900

    ✨add dummy data insertion util

--- src/main/java/com/sparta/taptoon/domain/matchingpost/enums/ArtistType.java
+++ src/main/java/com/sparta/taptoon/domain/matchingpost/enums/ArtistType.java
@@ -3,6 +3,7 @@ package com.sparta.taptoon.domain.matchingpost.enums;
 import com.sparta.taptoon.global.error.exception.InvalidRequestException;
 
 import java.util.Arrays;
+import java.util.Random;
 
 import static com.sparta.taptoon.global.error.enums.ErrorCode.INVALID_ARTIEST_TYPE;
 
@@ -11,11 +12,26 @@ public enum ArtistType {
 
     ;
 
+    // 예외 처리용
     public static ArtistType of(String type) {
         return Arrays.stream(values())
                 .filter(elem -> elem.name().equalsIgnoreCase(type))
                 .findFirst()
                 .orElseThrow(() -> new InvalidRequestException(INVALID_ARTIEST_TYPE));
     }
+
+    // QueryDSL 검색용
+    public static ArtistType fromString(String type) {
+        return Arrays.stream(values())
+                .filter(elem -> elem.name().equalsIgnoreCase(type))
+                .findFirst()
+                .orElse(null); // 일치하는 값이 없으면 null 반환
+    }
+
+    public static ArtistType random() {
+        ArtistType[] types = values();
+        return types[new Random().nextInt(values().length)];
+    }
+
 
 }
--- src/test/java/com/sparta/taptoon/domain/util/EntityCreatorUtil.java
+++ src/test/java/com/sparta/taptoon/domain/util/EntityCreatorUtil.java
@@ -1,11 +1,14 @@
 package com.sparta.taptoon.domain.util;
 
 import com.sparta.taptoon.domain.matchingpost.entity.MatchingPost;
+import com.sparta.taptoon.domain.matchingpost.entity.MatchingPostImage;
 import com.sparta.taptoon.domain.matchingpost.enums.ArtistType;
 import com.sparta.taptoon.domain.matchingpost.enums.WorkType;
 import com.sparta.taptoon.domain.member.entity.Member;
 import com.sparta.taptoon.domain.member.enums.MemberGrade;
 import lombok.experimental.UtilityClass;
+
+import java.text.MessageFormat;
 
 import static com.sparta.taptoon.domain.util.FakerUtil.faker;
 
@@ -26,10 +29,18 @@ public class EntityCreatorUtil {
     public static MatchingPost createMatchingPost(Member member) {
         return MatchingPost.builder()
                 .writer(member)
-                .artistType(ArtistType.ILLUSTRATOR)
+                .artistType(ArtistType.random())
                 .title(faker.lorem().word())
-                .workType(WorkType.HYBRID)
+                .workType(WorkType.random())
                 .description(faker.lorem().sentence())
                 .build();
     }
+
+    public static MatchingPostImage createMatchingPostImage(MatchingPost matchingPost) {
+        return MatchingPostImage.builder()
+                .matchingPost(matchingPost)
+                .imageUrl(MessageFormat.format("{0}/{1}", faker.internet().url(), faker.file().fileName()))
+                .build();
+    }
+
 }
