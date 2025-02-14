0f68b35a98794c40862974991f6d663522314390
chk223 <kch23_71@naver.com>
2025-02-14 15:37:18 +0900

    🐛 Category return 타입 수정

--- src/main/java/com/sparta/taptoon/domain/category/controller/CategoryController.java
+++ src/main/java/com/sparta/taptoon/domain/category/controller/CategoryController.java
@@ -1,13 +1,18 @@
 package com.sparta.taptoon.domain.category.controller;
 
+import com.sparta.taptoon.domain.category.entity.Category;
 import com.sparta.taptoon.domain.category.enums.Genre;
 import com.sparta.taptoon.domain.category.service.CategoryService;
 import com.sparta.taptoon.domain.member.entity.MemberDetail;
+import com.sparta.taptoon.global.common.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "Category", description = "카테고리 API")
 @RestController
@@ -19,20 +24,23 @@ public class CategoryController {
 
     @Operation(summary = "멤버의 카테고리(태그) 추가")
     @PostMapping
-    public void addMemberCategory(@AuthenticationPrincipal MemberDetail memberDetail, @RequestParam Genre genre) {
+    public ResponseEntity<ApiResponse<Void>> addMemberCategory(@AuthenticationPrincipal MemberDetail memberDetail, @RequestParam Genre genre) {
         categoryService.addMemberCategory(memberDetail.getId(), genre);
+        return ApiResponse.noContent();
     }
 
     @Operation(summary = "멤버의 카테고리 조회")
     @GetMapping
-    public void findMemberCategory(@AuthenticationPrincipal MemberDetail memberDetail) {
-        categoryService.findMemberCategory(memberDetail.getId());
+    public ResponseEntity<ApiResponse<List<Category>>> findMemberCategory(@AuthenticationPrincipal MemberDetail memberDetail) {
+        List<Category> memberCategory = categoryService.findMemberCategory(memberDetail.getId());
+        return ApiResponse.success(memberCategory);
     }
 
     @Operation(summary = "멤버의 카테고리 제거")
     @DeleteMapping
-    public void deleteMemberCategory(@AuthenticationPrincipal MemberDetail memberDetail, Genre genre) {
+    public ResponseEntity<ApiResponse<Void>> deleteMemberCategory(@AuthenticationPrincipal MemberDetail memberDetail, Genre genre) {
         categoryService.removeMemberCategory(memberDetail.getId(), genre);
+        return ApiResponse.noContent();
     }
 
 }
