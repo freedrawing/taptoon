52c3987dc6591644a14af885e7e3810b42c96c79
chk223 <kch23_71@naver.com>
2025-02-14 14:47:15 +0900

    ♻️ 컨트롤러에 Swagger 연결

--- src/main/java/com/sparta/taptoon/domain/auth/controller/AuthController.java
+++ src/main/java/com/sparta/taptoon/domain/auth/controller/AuthController.java
@@ -8,12 +8,15 @@ import com.sparta.taptoon.domain.auth.service.AuthService;
 import com.sparta.taptoon.domain.member.dto.response.MemberResponse;
 import com.sparta.taptoon.domain.member.entity.MemberDetail;
 import com.sparta.taptoon.global.common.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Auth", description = "인증 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
@@ -21,24 +24,28 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "회원 가입")
     @PostMapping("/sign-up")
     public ResponseEntity<ApiResponse<MemberResponse>> signUp(@RequestBody SignupMemberRequest request) {
         MemberResponse memberResponse = authService.signUp(request);
         return ApiResponse.created(memberResponse);
     }
 
+    @Operation(summary = "로그인")
     @PostMapping("/login")
     public ResponseEntity<ApiResponse<LoginMemberResponse>> login(@RequestBody LoginMemberRequest request, HttpServletRequest httpServletRequest) {
         LoginMemberResponse loginResponse = authService.login(request, httpServletRequest);
         return ApiResponse.success(loginResponse);
     }
 
+    @Operation(summary = "Access 토큰 재발급")
     @PostMapping("/refresh")
     public ResponseEntity<ApiResponse<TokenInfo>> refresh(@RequestHeader("Refresh-Token") String refreshToken) {
         TokenInfo tokenInfo = authService.issueAccessTokenByRefreshToken(refreshToken);
         return ApiResponse.success(tokenInfo);
     }
 
+    @Operation(summary = "로그아웃")
     @PostMapping("/logout")
     public void logout(@AuthenticationPrincipal MemberDetail memberDetail, HttpServletRequest httpServletRequest) {
         authService.logout(memberDetail.getId(), httpServletRequest);
--- src/main/java/com/sparta/taptoon/domain/category/controller/CategoryController.java
+++ src/main/java/com/sparta/taptoon/domain/category/controller/CategoryController.java
@@ -3,10 +3,13 @@ package com.sparta.taptoon.domain.category.controller;
 import com.sparta.taptoon.domain.category.enums.Genre;
 import com.sparta.taptoon.domain.category.service.CategoryService;
 import com.sparta.taptoon.domain.member.entity.MemberDetail;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Category", description = "카테고리 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/category")
@@ -14,16 +17,19 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
+    @Operation(summary = "멤버의 카테고리(태그) 추가")
     @PostMapping
     public void addMemberCategory(@AuthenticationPrincipal MemberDetail memberDetail, @RequestParam Genre genre) {
         categoryService.addMemberCategory(memberDetail.getId(), genre);
     }
 
+    @Operation(summary = "멤버의 카테고리 조회")
     @GetMapping
     public void findMemberCategory(@AuthenticationPrincipal MemberDetail memberDetail) {
         categoryService.findMemberCategory(memberDetail.getId());
     }
 
+    @Operation(summary = "멤버의 카테고리 제거")
     @DeleteMapping
     public void deleteMemberCategory(@AuthenticationPrincipal MemberDetail memberDetail, Genre genre) {
         categoryService.removeMemberCategory(memberDetail.getId(), genre);
