1e423db4f59ba888b119782c65b93df038f09a00
dllll2 <wlsdud8658@gmail.com>
2025-02-17 08:58:46 +0900

    ♻️엔티티, DTO 수정

--- src/main/java/com/sparta/taptoon/domain/chat/dto/request/SendChatMessageRequest.java
+++ src/main/java/com/sparta/taptoon/domain/chat/dto/request/SendChatMessageRequest.java
@@ -4,19 +4,13 @@ import com.sparta.taptoon.domain.chat.entity.ChatMessage;
 import com.sparta.taptoon.domain.chat.entity.ChatRoom;
 import com.sparta.taptoon.domain.member.entity.Member;
 
-public record SendChatMessageRequest(
-        Long chatRoomId,
-        Long senderId,
-        String message
+public record SendChatMessageRequest(Long chatRoomId, Long senderId, String message) {
 
-) {
-    public ChatMessage toEntity(ChatRoom chatRoom, Member sender) {
+    public static ChatMessage toEntity(SendChatMessageRequest request, ChatRoom chatRoom, Member sender) {
         return ChatMessage.builder()
                 .chatRoom(chatRoom)
                 .sender(sender)
-                .message(message)
-                .isRead(false)
-                .isDeleted(false)
+                .message(request.message())
                 .build();
     }
 }
