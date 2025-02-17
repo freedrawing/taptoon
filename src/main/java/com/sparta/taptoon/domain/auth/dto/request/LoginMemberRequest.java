107086b166b05983063541ffc0a9c587d2eed16d
chk223 <kch23_71@naver.com>
2025-02-17 09:32:33 +0900

    ♻️ 로그인/회원가입 request dto 에 validation 추가

--- src/main/java/com/sparta/taptoon/domain/auth/dto/request/LoginMemberRequest.java
+++ src/main/java/com/sparta/taptoon/domain/auth/dto/request/LoginMemberRequest.java
@@ -1,4 +1,10 @@
 package com.sparta.taptoon.domain.auth.dto.request;
 
-public record LoginMemberRequest(String email, String password) {
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginMemberRequest(
+        @NotBlank(message = "이메일을 입력하세요.")
+        String email,
+        @NotBlank(message = "비밀번호를 입력하세요.")
+        String password) {
 }
