9eda9611fd73ce0e691b226f31791326d0c0d620
dllll2 <wlsdud8658@gmail.com>
2025-02-17 09:13:25 +0900

    ✨ WebSocket 에서 JWT토큰 헤더를 사용하기위한 인터셉터 추가

--- src/main/java/com/sparta/taptoon/global/config/WebSocketConfig.java
+++ src/main/java/com/sparta/taptoon/global/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.sparta.taptoon.global.config;
 
+import com.sparta.taptoon.global.interceptor.WebSocketAuthInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
@@ -12,11 +13,13 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer {
 
-    private final WebSocketHandler WebSocketHandler;
+    private final WebSocketHandler webSocketHandler;
+    private final WebSocketAuthInterceptor webSocketAuthInterceptor;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(WebSocketHandler, "/ws/chat/{roomId}")
+        registry.addHandler(webSocketHandler, "/ws/chat/{roomId}")
+                .addInterceptors(webSocketAuthInterceptor)
                 .setAllowedOrigins("*"); // CORS 문제 해결
     }
 }
