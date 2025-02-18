b70c3e58f0f63fdcf570a42182e2249e4b1dcf47
seonguk_kang <dnr2144@gmail.com>
2025-02-18 16:59:13 +0900

    🎨add swagger annotation

--- src/main/java/com/sparta/taptoon/domain/matchingpost/controller/MatchingPostController.java
+++ src/main/java/com/sparta/taptoon/domain/matchingpost/controller/MatchingPostController.java
@@ -5,6 +5,8 @@ import com.sparta.taptoon.domain.matchingpost.dto.request.UpdateMatchingPostRequ
 import com.sparta.taptoon.domain.matchingpost.dto.response.MatchingPostResponse;
 import com.sparta.taptoon.domain.matchingpost.service.MatchingPostService;
 import com.sparta.taptoon.global.common.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -13,28 +15,30 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "matching-posts", description = "매칭보드 게시글 API")
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/matching-posts")
 public class MatchingPostController {
 
     private final MatchingPostService matchingPostService;
 
-    // 매칭 포스트 등록
-    @PostMapping("/matching-posts")
+    @Operation(summary = "매칭보드에 게시글 등록")
+    @PostMapping
     public ResponseEntity<ApiResponse<MatchingPostResponse>> createMatchingPost(@Valid @RequestBody AddMatchingPostRequest request) {
         MatchingPostResponse response = matchingPostService.makeNewMatchingPost(1L, request);
         return ApiResponse.created(response);
     }
 
-    // 매칭 포스트 단건 조회
-    @GetMapping("/matching-posts/{matchingPostId}")
+    @Operation(summary = "매칭 게시글 단건 조회")
+    @GetMapping("/{matchingPostId}")
     public ResponseEntity<ApiResponse<MatchingPostResponse>> getMatchingPost(@PathVariable Long matchingPostId) {
         MatchingPostResponse response = matchingPostService.findMatchingPostAndUpdateViewsV3(matchingPostId);
         return ApiResponse.success(response);
     }
 
-    // 매칭 포스트 수정 (일괄 수정)
-    @PutMapping("/matching-posts/{matchingPostId}")
+    @Operation(summary = "매칭 게시글 수정 (일괄 수정)")
+    @PutMapping("/{matchingPostId}")
     public ResponseEntity<ApiResponse<MatchingPostResponse>> updateMatchingPost(
             @PathVariable Long matchingPostId,
             @Valid @RequestBody UpdateMatchingPostRequest request) {
@@ -43,15 +47,15 @@ public class MatchingPostController {
         return ApiResponse.success(response);
     }
 
-    // 매칭 포스트 삭제 (soft deletion)
-    @DeleteMapping("/matching-posts/{matchingPostId}")
+    @Operation(summary = "매칭 게시글 삭제 (soft deletion)")
+    @DeleteMapping("/{matchingPostId}")
     public ResponseEntity<ApiResponse<Void>> deleteMatchingPost(@PathVariable Long matchingPostId) {
         matchingPostService.removeMatchingPost(1L, matchingPostId);
         return ApiResponse.success(null);
     }
 
-    // 매칭 포스트 다건 조회 (검색)
-    @GetMapping("/matching-posts")
+    @Operation(summary = "매칭 게시글 다건 조회 (검색)")
+    @GetMapping
     public ResponseEntity<ApiResponse<Page<MatchingPostResponse>>> getFilteredMatchingPosts(
             @RequestParam(required = false) String artistType,
             @RequestParam(required = false) String workType,
@@ -62,9 +66,4 @@ public class MatchingPostController {
         return ApiResponse.success(response);
     }
 
-    // 이미지랑 텍스트 업데이트 하는 API 추가로 만들어야 함
-    @GetMapping("/matching-posts/file")
-    public ResponseEntity<ApiResponse<Void>> uploadFile() {
-        return null;
-    }
 }
