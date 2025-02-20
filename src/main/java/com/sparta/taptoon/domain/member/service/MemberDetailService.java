dac2f7d77156581fe8d628316f3d056631081de1
chk223 <kch23_71@naver.com>
2025-02-20 10:27:00 +0900

    ♻️ Member SoftDeletion 검증하도록 수정

--- src/main/java/com/sparta/taptoon/domain/member/service/MemberDetailService.java
+++ src/main/java/com/sparta/taptoon/domain/member/service/MemberDetailService.java
@@ -5,6 +5,7 @@ import com.sparta.taptoon.domain.member.dto.MemberDetail;
 import com.sparta.taptoon.domain.member.enums.OAuthProvider;
 import com.sparta.taptoon.domain.member.repository.MemberRepository;
 import com.sparta.taptoon.global.error.enums.ErrorCode;
+import com.sparta.taptoon.global.error.exception.AccessDeniedException;
 import com.sparta.taptoon.global.error.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,14 +26,15 @@ public class MemberDetailService implements UserDetailsService {
         Member member = null;
         // 이메일 형식인지 확인
         if (isValidEmail(userIdentification)) {
-            log.info("이메일로 로그인함! email: {}",userIdentification);
+            log.info("이메일로 로그인함! email: {}", userIdentification);
             member = memberRepository.findByEmail(userIdentification)
                     .orElseThrow(() -> new UsernameNotFoundException("해당 이메일을 가진 사용자를 찾을 수 없습니다."));
-            return new MemberDetail(member);
+        } else {
+            log.info("소셜 로그인함! providerId: {}", userIdentification);
+            member = memberRepository.findByProviderId(userIdentification)
+                    .orElseThrow(() -> new NotFoundException(ErrorCode.MEMBER_NOT_FOUND));
         }
-        log.info("소셜 로그인함! providerId: {}", userIdentification);
-        member = memberRepository.findByProviderId(userIdentification)
-                .orElseThrow(() -> new NotFoundException(ErrorCode.MEMBER_NOT_FOUND));
+        if(member.getIsDeleted()) throw new AccessDeniedException(ErrorCode.MEMBER_DELETED);
         return new MemberDetail(member);
     }
     private boolean isValidEmail(String email) {
