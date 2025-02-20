a46a735754854bf6773c856d0f1afc46e6a0fe9c
dllll2 <wlsdud8658@gmail.com>
2025-02-20 14:23:48 +0900

    ✨ 채팅방 목록 조회기능, 채팅방 삭제기능 추가

--- src/main/java/com/sparta/taptoon/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/sparta/taptoon/domain/chat/repository/ChatMessageRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
@@ -14,5 +15,11 @@ public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> 
 
     /** 특정 메시지 ID 이후의 안 읽은 메시지 조회 */
     List<ChatMessage> findByChatRoomAndIdGreaterThan(ChatRoom chatRoom, Long lastReadMessageId);
+
+    /** 채팅방 마지막 메시지 조회 */
+    Optional<ChatMessage> findTopByChatRoomOrderByCreatedAtDesc(ChatRoom chatRoom);
+
+    /** 마지막으로 읽은 메시지 이후 메시지 개수 조회 */
+    int countByChatRoomAndIdGreaterThan(ChatRoom chatRoom, Long lastReadMessageId);
 
 }
