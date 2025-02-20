d4f1f37b0066c1e370bf595b9a4343477d946c97
chk223 <kch23_71@naver.com>
2025-02-20 16:45:53 +0900

    ✨ dto 사이즈 추가

--- src/main/java/com/sparta/taptoon/domain/auth/dto/request/SignupMemberRequest.java
+++ src/main/java/com/sparta/taptoon/domain/auth/dto/request/SignupMemberRequest.java
@@ -4,16 +4,20 @@ import com.sparta.taptoon.domain.member.entity.Member;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Builder
 public record SignupMemberRequest(
+        @Size(max = 25)
         @Email(message = "이메일 형식이 올바르지 않습니다.")
         @NotBlank(message = "이메일을 입력하세요.")
         String email,
+        @Size(max = 10)
         @NotBlank(message = "이름을 입력하세요.")
         String name,
+        @Size(max = 10)
         @NotBlank(message = "닉네임을 입력하세요.")
         String nickname,
         @Pattern(
