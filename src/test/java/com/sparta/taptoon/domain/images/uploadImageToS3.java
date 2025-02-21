90df56c913188a7f0e0bfb90b21c2d2f3e66bf69
chk223 <104356399+chk223@users.noreply.github.com>
2025-02-21 14:08:29 +0900

    ✅이미지 upload 테스트 Mock 사용하도록 변경

--- src/test/java/com/sparta/taptoon/domain/images/uploadImageToS3.java
+++ src/test/java/com/sparta/taptoon/domain/images/uploadImageToS3.java
@@ -1,31 +1,39 @@
 package com.sparta.taptoon.domain.images;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.sparta.taptoon.domain.image.S3UploadClient;
 import com.sparta.taptoon.domain.image.service.ImageService;
-import feign.Client;
-import feign.Feign;
-import feign.codec.Decoder;
-import feign.codec.Encoder;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.openfeign.support.SpringMvcContract;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.util.FileCopyUtils;
 
 import java.io.IOException;
+import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
-@SpringBootTest
 @ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
 public class uploadImageToS3 {
-    @Autowired
+    @InjectMocks
     private ImageService imageService;
 
-    @DisabledIfSystemProperty(named = "CI", matches = "true")
+    @Mock
+    S3UploadClient s3UploadClient;
+
+    @Mock
+    private AmazonS3 amazonS3;
+
     @Test
     void uploadImageUsingFeign() throws IOException {
         // given
@@ -35,24 +43,27 @@ public class uploadImageToS3 {
         String directory_t = "test";
         String directory = directory_t;
         String fileName = "test-image.jpg";
+        String mockPreSignedUrl = "https://test-bucket.s3.amazonaws.com/test/test-image.jpg";
 
-        String presignedUrl = imageService.generatePresignedUrl(directory, fileName);
+        // Mock URL 객체 생성
+        URL mockUrl = new URL(mockPreSignedUrl);
 
-        S3UploadClient s3UploadClient = Feign.builder()
-                .contract(new SpringMvcContract())
-                .client(new Client.Default(null, null))
-                .encoder(new Encoder.Default())
-                .decoder(new Decoder.Default())
-                .target(S3UploadClient.class, presignedUrl);
+        // amazonS3 mock 설정
+        when(amazonS3.generatePresignedUrl(any())).thenReturn(mockUrl);
+        doNothing()
+                .when(s3UploadClient)
+                .uploadFile(eq("image/jpeg"), any(byte[].class));
+        //when
+        String preSignedUrl = imageService.generatePresignedUrl(directory, fileName);
 
         // 테스트 이미지 로드
         ClassPathResource resource = new ClassPathResource("test-images/test-image.jpg");
         byte[] imageBytes = FileCopyUtils.copyToByteArray(resource.getInputStream());
 
         // 파일 업로드 - 예외가 발생하지 않으면 성공
-        assertDoesNotThrow(() -> s3UploadClient.uploadFile(
-                "image/jpeg",
-                imageBytes
-        ));
+        assertDoesNotThrow(() -> s3UploadClient.uploadFile("image/jpeg", imageBytes));
+        assertEquals(mockUrl.toString(), preSignedUrl);
+        verify(s3UploadClient).uploadFile(eq("image/jpeg"), any(byte[].class));
+        verify(amazonS3).generatePresignedUrl(any(GeneratePresignedUrlRequest.class));
     }
 }
