bef006682269e5be35e178877dac4b07308768cc
chk223 <kch23_71@naver.com>
2025-02-23 23:21:02 +0900

    ♻️ WebClient -> OpenFeign 으로 변경

--- src/main/java/com/sparta/taptoon/global/config/FeignConfig.java
+++ src/main/java/com/sparta/taptoon/global/config/FeignConfig.java
@@ -1,13 +1,33 @@
 package com.sparta.taptoon.global.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import feign.Client;
+import feign.Logger;
 import feign.codec.Decoder;
 import feign.codec.Encoder;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() { // Param은 직렬화/역직렬화가 안됨..
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return objectMapper;
+    }
+
     @Bean
     public Client feignClient() {
         return new Client.Default(null, null);
@@ -15,11 +35,11 @@ public class FeignConfig {
 
     @Bean
     public Decoder feignDecoder() {
-        return new Decoder.Default();
+        return new JacksonDecoder(objectMapper());
     }
 
     @Bean
     public Encoder feignEncoder() {
-        return new Encoder.Default();
+        return new JacksonEncoder(objectMapper());
     }
 }
