c2d165b02d41aecc60ea5e5c8e20b278561548ef
chk223 <kch23_71@naver.com>
2025-02-24 01:59:50 +0900

    ♻️ObjectMapper 를 WebConfig에서 관리하도록 수정

--- src/main/java/com/sparta/taptoon/global/config/WebConfig.java
+++ src/main/java/com/sparta/taptoon/global/config/WebConfig.java
@@ -1,8 +1,11 @@
 package com.sparta.taptoon.global.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -11,14 +14,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
+    }
+
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());// Java 8의 날짜/시간 타입(LocalDateTime 등)을 처리할 수 있는 모듈 등록
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);// 날짜를 타임스탬프가 아닌 ISO-8601 형식의 문자열로 직렬화하도록 설정
-        // 위에서 설정한 ObjectMapper를 사용하는 새로운 HTTP 메시지 변환기를 생성
         MappingJackson2HttpMessageConverter converter =
-                new MappingJackson2HttpMessageConverter(objectMapper);
-        converters.add(0, converter);// 생성한 변환기를 변환기 목록의 가장 앞에 추가
+                new MappingJackson2HttpMessageConverter(objectMapper());
+        converters.add(0, converter);
     }
 }
