60ccc5ad4261225ff7da64dffbdf49644ce1670f
chk223 <kch23_71@naver.com>
2025-02-24 14:38:38 +0900

    ♻️이미지 처리 chat 버전 오버라이딩을 위해 인터페이스 추가

--- src/main/java/com/sparta/taptoon/domain/image/controller/ImageController.java
+++ src/main/java/com/sparta/taptoon/domain/image/controller/ImageController.java
@@ -2,14 +2,16 @@ package com.sparta.taptoon.domain.image.controller;
 
 import com.sparta.taptoon.domain.image.dto.PreSignedUrlRequest;
 import com.sparta.taptoon.domain.image.service.ImageService;
-import com.sparta.taptoon.domain.matchingpost.service.MatchingPostService;
 import com.sparta.taptoon.global.common.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "images", description = "이미지 업로드 API")
 @RestController
