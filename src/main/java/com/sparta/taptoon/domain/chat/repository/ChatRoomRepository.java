1399edc4de195db9f903e3f2d93a0f65267c8680
dllll2 <wlsdud8658@gmail.com>
2025-02-25 11:52:09 +0900

    🐛 같은 인원의 채팅방을 중복으로 계속 만들수 있는 버그 수정

--- src/main/java/com/sparta/taptoon/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/sparta/taptoon/domain/chat/repository/ChatRoomRepository.java
@@ -1,13 +1,13 @@
 package com.sparta.taptoon.domain.chat.repository;
 
 import com.sparta.taptoon.domain.chat.entity.ChatRoom;
-import com.sparta.taptoon.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
@@ -16,4 +16,12 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
     @Query("SELECT c.id FROM ChatRoom c WHERE c.isDeleted = false ")
     List<Long> findChatRoomIds();
+
+    @Query("SELECT cr FROM ChatRoom cr JOIN cr.members crm " +
+            "WHERE cr.isDeleted = false " +
+            "GROUP BY cr " +
+            "HAVING COUNT(crm.member) = :memberCount " +
+            "AND SUM(CASE WHEN crm.member.id IN :memberIds THEN 1 ELSE 0 END) = :memberCount " +
+            "AND SUM(CASE WHEN crm.member.id NOT IN :memberIds THEN 1 ELSE 0 END) = 0")
+    Optional<ChatRoom> findByExactMembers(@Param("memberIds") List<Long> memberIds, @Param("memberCount") int memberCount);
 }
