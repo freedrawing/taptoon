00fe1ea8c6c8fd87bdced89eb5994bdfc114793b
239 <rufwjd12@gmail.com>
2025-02-28 21:48:52 +0900

    ♻️ '포트폴리오 내용 채우기' api 수정 시나리오 로직 수정 및 추가

--- src/main/java/com/sparta/taptoon/domain/portfolio/service/PortfolioService.java
+++ src/main/java/com/sparta/taptoon/domain/portfolio/service/PortfolioService.java
@@ -9,6 +9,7 @@ import com.sparta.taptoon.domain.portfolio.entity.Portfolio;
 import com.sparta.taptoon.domain.portfolio.entity.PortfolioImage;
 import com.sparta.taptoon.domain.portfolio.repository.PortfolioImageRepository;
 import com.sparta.taptoon.domain.portfolio.repository.PortfolioRepository;
+import com.sparta.taptoon.global.common.enums.Status;
 import com.sparta.taptoon.global.error.enums.ErrorCode;
 import com.sparta.taptoon.global.error.exception.CreationLimitExceededException;
 import com.sparta.taptoon.global.error.exception.NotFoundException;
@@ -45,7 +46,6 @@ public class PortfolioService {
                 .member(member)
                 .title("")
                 .content("")
-                .fileUrl("")
                 .build();
         Portfolio savedPortfolio = portfolioRepository.save(portfolio);
         return savedPortfolio.getId();
@@ -58,23 +58,37 @@ public class PortfolioService {
         // 내용 채울 포트폴리오 찾기
         Portfolio foundPortfolio = portfolioRepository.findById(portfolioId)
                 .orElseThrow(() -> new NotFoundException(ErrorCode.PORTFOLIO_NOT_FOUND));
-
         //  제목 + 내용 기입하기
         foundPortfolio.updatePortfolio(portfolioRequest);
 
-        // 이미지 등록
-        List<PortfolioImageResponse> portfolioFileResponses = Collections.emptyList();
-        if (!portfolioRequest.fileIds().isEmpty()) {
-            // 레포지에 등록된 파일 모두 찾기
-            List<PortfolioImage> foundFiles = portfolioImageRepository.findAllById(portfolioRequest.fileIds());
-            // pending에서 registered로 상태 변환
-            foundFiles.forEach(PortfolioImage::updateStatus);
-
-            List<PortfolioImageResponse> portfolioImageResponses = foundFiles.stream()
-                    .map(PortfolioImageResponse::from) // Status registered로 변경
-                    .collect(Collectors.toList());
+        // 이미지 아이디 값이 null일 때 빈리스트로 반환(npe 방지)
+        if (portfolioRequest.fileIds() == null || portfolioRequest.fileIds().isEmpty()) {
+            return PortfolioResponse.from(foundPortfolio, Collections.emptyList());
         }
-        return PortfolioResponse.from(foundPortfolio, portfolioFileResponses);
+
+        // 이미지 파일 등록 및 수정 메서드
+        // 요청된 파일(pending) 모두 찾기
+        List<PortfolioImage> foundFiles = portfolioImageRepository.findAllById(portfolioRequest.fileIds()).stream()
+                .filter(file -> file.getStatus() == Status.PENDING) // Pending 상태인 파일만 (중복 방지)
+                .collect(Collectors.toList());
+        // pending에서 registered로 상태 변환
+        foundFiles.forEach(PortfolioImage::updateStatus);
+
+        // 삭제할 이미지 찾기 (request에 포함되어있지않은 이미지 삭제)
+        List<PortfolioImage> portfolioImages = portfolioImageRepository.findAllByPortfolioId(portfolioId);
+        List<PortfolioImage> toDeleteFile = portfolioImages.stream()
+                .filter(file -> !portfolioRequest.fileIds().contains(file.getId()))
+                .collect(Collectors.toList());
+        // registered에서 deleting으로 상태 변환
+        toDeleteFile.forEach(PortfolioImage::removeFile);
+
+        // 위의 모든 로직 처리 후 포트폴리오에 최종 이미지들 response에 담기
+        List<PortfolioImage> finalPortfolioImages = portfolioImageRepository.findAllById(portfolioRequest.fileIds());
+        List<PortfolioImageResponse> portfolioImageResponses =  finalPortfolioImages.stream()
+                .map(PortfolioImageResponse::from)
+                .collect(Collectors.toList());
+
+        return PortfolioResponse.from(foundPortfolio, portfolioImageResponses);
     }
 
 
@@ -84,7 +98,7 @@ public class PortfolioService {
                 .orElseThrow(() -> new NotFoundException(ErrorCode.PORTFOLIO_NOT_FOUND));
 
         // 포트폴리오 이미지를 포트폴리오 id로 찾기
-        List<PortfolioImage> portfolioImages = portfolioImageRepository.findByPortfolioId(portfolioId);
+        List<PortfolioImage> portfolioImages = portfolioImageRepository.findAllByPortfolioId(portfolioId);
         // 포트폴리오 이미지 responseDto에 담기
         List<PortfolioImageResponse> portfolioImageResponses = portfolioImages.stream()
                 .map(PortfolioImageResponse::from)
@@ -97,18 +111,17 @@ public class PortfolioService {
     // 포트폴리오 삭제
     @Transactional
     public void removePortfolio(Long portfolioId, Member member) {
-
         // 삭제할 포트폴리오 Id로 찾기
         Portfolio portfolio = portfolioRepository.findById(portfolioId)
                 .orElseThrow(() -> new NotFoundException(ErrorCode.PORTFOLIO_NOT_FOUND));
 
-        // 삭제할 포트폴리오가 유저의 포트폴리오인지 검사
-        if(!portfolio.getMember().getId().equals(member.getId())) {
-            throw new NotFoundException(ErrorCode.PORTFOLIO_ACCESS_DENIED);
-        }
-
         // 포트폴리오 삭제 (boolean type true로 변경)
         portfolio.remove();
 
+        // 포트폴리오 삭제시 이미지도 완전 삭제
+            List<PortfolioImage> portfolioImages = portfolioImageRepository.findAllByPortfolioId(portfolioId);
+            if (!portfolioImages.isEmpty()) {
+                portfolioImageRepository.deleteAll(portfolioImages);
+            }
     }
 }
