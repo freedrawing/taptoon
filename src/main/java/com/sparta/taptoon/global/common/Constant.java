08c60a5d43a0590711dad9ef0f5fa3e4c81777b4
seonguk_kang <dnr2144@gmail.com>
2025-03-02 00:10:57 +0900

    ♻️ Constant 및 ContentTypeUtil 추가
    공통 상수 담아놓는 클래스와 ContentType Filter 기능

--- src/main/java/com/sparta/taptoon/global/common/Constant.java
+++ src/main/java/com/sparta/taptoon/global/common/Constant.java
@@ -3,19 +3,16 @@ package com.sparta.taptoon.global.common;
 import lombok.Getter;
 
 @Getter
-public enum Constant {
+public class Constant {
 
     // About S3
-    S3_ORIGINAL_IMAGE_PATH("original"),
-    S3_THUMBNAIL_IMAGE_PATH("thumbnail"),
-    S3_MATCHING_POST_IMAGE_PATH("matchingpost"),
-    S3_PORTFOLIO_IMAGE_PATH("portfolio"),
+    public static String S3_ORIGINAL_IMAGE_PATH = "original";
+    public static String S3_THUMBNAIL_IMAGE_PATH = "thumbnail";
+    public static String S3_MATCHING_POST_IMAGE_PATH = "matchingpost";
+    public static String S3_PORTFOLIO_IMAGE_PATH = "portfolio";
 
-    ;
+    // FileType
+    public static String FILE_TYPE = "file";
+    public static String IMAGE_TYPE = "image";
 
-    Constant(String stringConstant) {
-        this.stringConstant = stringConstant;
-    }
-
-    private final String stringConstant;
 }
