fb90b736c030ff0937358190bbb2510bf878224e
seonguk_kang <dnr2144@gmail.com>
2025-03-02 00:09:19 +0900

    ♻️ image or file 추가할 때, Request fileType 필드 추가

--- src/main/java/com/sparta/taptoon/domain/image/dto/request/PreSignedUrlRequest.java
+++ src/main/java/com/sparta/taptoon/domain/image/dto/request/PreSignedUrlRequest.java
@@ -7,5 +7,6 @@ import jakarta.validation.constraints.Pattern;
 public record PreSignedUrlRequest(
         @NotBlank(message = "폴더 이름은 필수입니다.") @Pattern(regexp = "^[a-zA-Z0-9/._-]+$", message = "올바르지 않은 폴더 이름입니다.") String directory,
         @NotNull(message = "id 값은 필수입니다.") Long id,
+        @NotBlank(message = "fileType은 필수값입니다.") String fileType,
         @NotBlank(message = "파일 이름은 필수입니다.") String fileName) {
 }
