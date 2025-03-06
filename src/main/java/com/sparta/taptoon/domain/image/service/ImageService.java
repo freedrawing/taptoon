432d7099bd6bd91f4d424edc852f404c74601255
dllll2 <wlsdud8658@gmail.com>
2025-03-06 17:54:30 +0900

    ♻️ DB 변경에따른 매개변수 타입 수정

--- src/main/java/com/sparta/taptoon/domain/image/service/ImageService.java
+++ src/main/java/com/sparta/taptoon/domain/image/service/ImageService.java
@@ -4,7 +4,7 @@ import com.sparta.taptoon.domain.image.dto.response.PresignedUrlResponse;
 
 public interface ImageService {
     PresignedUrlResponse generatePresignedUrl(String folderPath, Long id, String fileType, String fileName);
-    String generatePresignedUrl(String folderPath, Long roomId, Long memberId, String fileName);
+    String generatePresignedUrl(String folderPath, String roomId, Long memberId, String fileName);
     void removeFileFromS3(String imageUrl);
 
 }
